456391870ab3b180b1f13326ceaede0887404ec4
weiximei <907147608@qq.com>
2022-07-18 22:15:16 +0800

    提交

--- yqm-shop-common/src/main/java/com/yqm/constant/ShopConstants.java
+++ yqm-shop-common/src/main/java/com/yqm/constant/ShopConstants.java
@@ -37,26 +37,26 @@ public interface ShopConstants {
 	/**
 	 * 微信支付service
 	 */
-	String YQM_SHOP_WEIXIN_PAY_SERVICE = "YQM_SHOP_weixin_pay_service";
+	String YQM_SHOP_WEIXIN_PAY_SERVICE = "yqm-shop_weixin_pay_service";
 
 	/**
 	 * 微信支付小程序service
 	 */
-	String YQM_SHOP_WEIXIN_MINI_PAY_SERVICE = "YQM_SHOP_weixin_mini_pay_service";
+	String YQM_SHOP_WEIXIN_MINI_PAY_SERVICE = "yqm-shop_weixin_mini_pay_service";
 
 	/**
 	 * 微信支付app service
 	 */
-	String YQM_SHOP_WEIXIN_APP_PAY_SERVICE = "YQM_SHOP_weixin_app_pay_service";
+	String YQM_SHOP_WEIXIN_APP_PAY_SERVICE = "yqm-shop_weixin_app_pay_service";
 
 	/**
 	 * 微信公众号service
 	 */
-	String YQM_SHOP_WEIXIN_MP_SERVICE = "YQM_SHOP_weixin_mp_service";
+	String YQM_SHOP_WEIXIN_MP_SERVICE = "yqm-shop_weixin_mp_service";
 	/**
 	 * 微信小程序service
 	 */
-	String YQM_SHOP_WEIXIN_MA_SERVICE = "YQM_SHOP_weixin_ma_service";
+	String YQM_SHOP_WEIXIN_MA_SERVICE = "yqm-shop_weixin_ma_service";
 
 	/**
 	 * 商城默认密码
@@ -76,45 +76,45 @@ public interface ShopConstants {
 	/**
 	 * redis首页键
 	 */
-	String YQM_SHOP_REDIS_INDEX_KEY = "YQM_SHOP:index_data";
+	String YQM_SHOP_REDIS_INDEX_KEY = "yqm-shop:index_data";
 
 	/**
 	 * 配置列表缓存
 	 */
-	String YQM_SHOP_REDIS_CONFIG_DATAS = "YQM_SHOP:config_datas";
+	String YQM_SHOP_REDIS_CONFIG_DATAS = "yqm-shop:config_datas";
 
 	/**
 	 * 充值方案
 	 */
-	String YQM_SHOP_RECHARGE_PRICE_WAYS = "YQM_SHOP_recharge_price_ways";
+	String YQM_SHOP_RECHARGE_PRICE_WAYS = "yqm-shop_recharge_price_ways";
 	/**
 	 * 首页banner
 	 */
-	String YQM_SHOP_HOME_BANNER = "YQM_SHOP_home_banner";
+	String YQM_SHOP_HOME_BANNER = "yqm-shop_home_banner";
 	/**
 	 * 首页菜单
 	 */
-	String YQM_SHOP_HOME_MENUS = "YQM_SHOP_home_menus";
+	String YQM_SHOP_HOME_MENUS = "yqm-shop_home_menus";
 	/**
 	 * 首页滚动新闻
 	 */
-	String YQM_SHOP_HOME_ROLL_NEWS = "YQM_SHOP_home_roll_news";
+	String YQM_SHOP_HOME_ROLL_NEWS = "yqm-shop_home_roll_news";
 	/**
 	 * 热门搜索
 	 */
-	String YQM_SHOP_HOT_SEARCH = "YQM_SHOP_hot_search";
+	String YQM_SHOP_HOT_SEARCH = "yqm-shop_hot_search";
 	/**
 	 * 个人中心菜单
 	 */
-	String YQM_SHOP_MY_MENUES = "YQM_SHOP_my_menus";
+	String YQM_SHOP_MY_MENUES = "yqm-shop_my_menus";
 	/**
 	 * 秒杀时间段
 	 */
-	String YQM_SHOP_SECKILL_TIME = "YQM_SHOP_seckill_time";
+	String YQM_SHOP_SECKILL_TIME = "yqm-shop_seckill_time";
 	/**
 	 * 签到天数
 	 */
-	String YQM_SHOP_SIGN_DAY_NUM = "YQM_SHOP_sign_day_num";
+	String YQM_SHOP_SIGN_DAY_NUM = "yqm-shop_sign_day_num";
 
 	/**
 	 * 打印机配置
@@ -155,25 +155,25 @@ public interface ShopConstants {
 
 	int YQM_SHOP_ONE_NUM = 1;
 
-	String YQM_SHOP_ORDER_CACHE_KEY = "YQM_SHOP:order";
+	String YQM_SHOP_ORDER_CACHE_KEY = "yqm-shop:order";
 
 	long YQM_SHOP_ORDER_CACHE_TIME = 600L;
 
 	String WECHAT_MENUS =  "wechat_menus";
 
-	String YQM_SHOP_EXPRESS_SERVICE = "YQM_SHOP_express_service";
+	String YQM_SHOP_EXPRESS_SERVICE = "yqm-shop_express_service";
 
-	String YQM_SHOP_REDIS_SYS_CITY_KEY = "YQM_SHOP:city_list";
+	String YQM_SHOP_REDIS_SYS_CITY_KEY = "yqm-shop:city_list";
 
-	String YQM_SHOP_REDIS_CITY_KEY = "YQM_SHOP:city";
+	String YQM_SHOP_REDIS_CITY_KEY = "yqm-shop:city";
 
 	String YQM_SHOP_APP_LOGIN_USER = "app-online-token:";
 
-	String YQM_SHOP_WECHAT_PUSH_REMARK = "YQM_SHOP为您服务！";
+	String YQM_SHOP_WECHAT_PUSH_REMARK = "yqm-shop为您服务！";
 
 	String DEFAULT_UNI_H5_URL = "https://h5.yqm.co";
 
-	String YQM_SHOP_MINI_SESSION_KET = "YQM_SHOP:session_key:";
+	String YQM_SHOP_MINI_SESSION_KET = "yqm-shop:session_key:";
 
 	/**公众号二维码*/
 	String WECHAT_FOLLOW_IMG="wechat_follow_img";
